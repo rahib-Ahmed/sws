c17f76245645c2e48878e705f50814faa91ec2c4
Rahib uzair ahmed <rahibsam2@gmail.com>
2021-04-01 20:58:56 +0530

    integrated fetch

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Piechart from './components/Piechart'
 import Footer from './components/Footer'
 import Carouseltext from './components/Carousl';
 import Tabsbutton from './components/Tabs';
 import Header from './components/Header'
-
-
-
-// function getTrashId() {
-//   const query = window.location.search;
-//   const idParam = new URLSearchParams(query)
-
-//   const id = idParam.get('trashid')
-
-//   var searchParams = new URLSearchParams();
-//   searchParams.append("id", id);
-//   var header = new Headers()
-//   header.append("Content-Type", "application/x-www-form-urlencoded")
-
-//   var res = {
-//     method: 'POST',
-//     header: header,
-//     body: searchParams
-//   }
-
-//   fetch('https://helpsws.herokuapp.com/scanned', res)
-//   .then(res => res.json())
-//   .then(result => console.log(result))
-// }
-// getTrashId()
+import * as fetch from './backend/fetch.js'
 
 function App() {
+
+useEffect(() => {
+  fetch.getTrashId()   
+})
 
   return ( 
      
