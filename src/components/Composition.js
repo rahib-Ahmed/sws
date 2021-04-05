1a0510617e0ff10be13a8f316e538d466c112968
Rahib uzair ahmed <rahibsam2@gmail.com>
2021-04-05 22:28:57 +0530

    new Tab Components

---
+++ src/components/Composition.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+ 
+
+function Composition(){
+    return(
+        <div>
+          Thr
+        </div>
+    )
+}
+
+export default Composition;
---
+++ src/components/Storage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Storage(){
+    return(
+        <div>
+hello
+        </div>
+    )
+}
+
+export default Storage;
