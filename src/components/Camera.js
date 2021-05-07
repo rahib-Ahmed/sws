d8864adec2f76919f705bc282177c785bcbc6ba3
Rahib uzair ahmed <52162363+samxoxoxo@users.noreply.github.com>
2021-05-08 02:12:48 +0530

    added a wait screen

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -10,21 +10,32 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import Header from './Header'
 import Footer from "./Footer";
-
+import * as fetchs from '../backend/fetch'
+import { useHistory } from "react-router-dom";
 
 function Camera() {
   
   const [delay,setDelay] = React.useState(300);
   const [result,setResult] = React.useState('Plas scan properly');
   const [open, setOpen] = React.useState(false);
-
+  const history = useHistory();
   const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
 
   const handleLink = () => {
-    localStorage.setItem("check", "true")
-    window.location.replace(result)
+    fetch('https://helpsws.herokuapp.com/userdata', fetchs.updateUserTrack())
+    .then((res) => res.json())
+    .then((results) => {
+    
+      localStorage.setItem("check", "true")
+      fetch('https://helpsws.herokuapp.com/updateusertrack', fetchs.updateUserTrack("scanned"))
+      .then(res => res.json())
+      .then(resu => window.location.replace(result) )
+      
+     })
+    
+    .catch(err=>console.log(err))
   }; 
   const handleClose = () =>{
     setOpen(false)
@@ -53,7 +64,6 @@ function Camera() {
         <Footer />
         </div>
 
-
           <div>
           <Dialog
         open={open}
@@ -63,15 +73,17 @@ function Camera() {
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle id="alert-dialog-slide-title">{"Plz follow the link for trash bin data"}</DialogTitle>
+        <DialogTitle id="alert-dialog-slide-title"><div className="modHead">{"Please follow the link to the app."}</div></DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-slide-description">
-            {result}
+            <div className="modFont">{result}</div>
           </DialogContentText>
         </DialogContent>
         <DialogActions>{ result==="trash_link_here"?<Button disabled={true} >Open Link</Button> :
           <Button onClick={handleLink} color="primary">
+            <div className="openlinkStyle">
             Open Link
+            </div>
           </Button>}
         </DialogActions>
       </Dialog>
